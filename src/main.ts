1715f0d812542012058c13db3182e9827de703f8
Raghav Misra <raghav.m2014@gmail.com>
2022-01-06 18:22:17 -0800

    stop cdn loading iconify & bulma

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@iconify/iconify";
 //@ts-ignore
 import { registerSW } from "virtual:pwa-register";
 registerSW({ immediate: true });
