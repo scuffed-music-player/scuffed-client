f4779790cd7e2466b5f807a60a23866db254aaeb
Raghav Misra <raghav.m2014@gmail.com>
2022-02-10 21:54:36 -0800

    omg okay song resolution is sm better now

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -15,7 +15,7 @@ async function loadSong() {
 
     const q = encodeURIComponent(query.value.trim().toLowerCase());
 
-    const dataResponse = await request("GET")(`/api/search/${q}%20audio`);
+    const dataResponse = await request("GET")(`/api/search/${q}%20song`);
     const videoData = await dataResponse.json() as { success: boolean, song: ISongData };
 
     if (!videoData.success) {
