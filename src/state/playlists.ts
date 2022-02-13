77ac9e6504d3b5b877353968677a40bd2569a3df
Raghav Misra <raghav.m2014@gmail.com>
2022-02-12 18:13:35 -0800

    Amazing, playlists are serverside now!

--- src/state/playlists.ts
+++ src/state/playlists.ts
@@ -1,5 +1,5 @@
-import localforage from "localforage";
 import { ref, watchEffect, computed } from "vue";
+import { request } from "../helpers/request";
 import { ISongData } from "./player";
 
 export interface IPlaylist {
@@ -16,8 +16,14 @@ export const currentPlaylistId = ref("");
 export const currentPlaylist = computed(() => playlists.value.find(p => p._id == currentPlaylistId.value));
 
 (async () => {
-    playlists.value = await localforage.getItem<IPlaylist[]>("playlists") || [];
-    currentPlaylistId.value = playlists.value[0]?._id || "";
+    const playlistsRes = await request("GET")("/api/playlists");
+    const { success, playlists: serverPlaylists } = await playlistsRes.json();
+    if (success) {
+        playlists.value = serverPlaylists;
+        currentPlaylistId.value = playlists.value[0]?._id || "";
+    }
+    
+    watchEffect(() => request("POST")("/api/playlists", {
+        body: JSON.stringify({ playlists: playlists.value })
+    }));
 })();
-
-watchEffect(() => localforage.setItem("playlists", JSON.parse(JSON.stringify(playlists.value))));
